1c939d62626c39a1d5e9ae926c8a98f7cc2811ea
Tom LE GROS <freezekaz.dement@gmail.com>
2023-09-30 17:17:49 +0200

    setup gh-pages

--- src/App.js
+++ src/App.js
@@ -35,9 +35,9 @@ class App extends React.Component {
 		return (
 			<Router>
 				<Header/>
-
+				<Routes>
 					<Route path="/" element={<Home/>} />
-
+					</Routes>
 				<Footer/>
 			</Router>
 		);
